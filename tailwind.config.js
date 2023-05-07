0befeddeff281cb8943ebda9f98574f7fc89cca4
cmdm2022 <camiladuartemaidana@gmail.com>
2023-05-07 00:08:56 -0400

    Front-end

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+      
+        puce: {
+          '50': '#faf5f8',
+          '100': '#f6edf2',
+          '200': '#efdbe6',
+          '300': '#e2bfd1',
+          '400': '#d096b3',
+          '500': '#bf789a',
+          '600': '#a9597c',
+          '700': '#8f4764',
+          '800': '#773d53',
+          '900': '#653648',
+          '950': '#3c1b28',
+      },
+      
+      }
+    }
+  },
+  plugins: [],
+}
+
